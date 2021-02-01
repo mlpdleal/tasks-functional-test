6699d15c79c49d2fcda79be53b4cd9c02e60db58
mlpdleal <manoellealdeveloper@gmail.com>
2021-02-01 19:03:05 -0300

    Atualizando

--- src/test/java/br/com/mlpdleal/tasks/functional/TasksTest.java
+++ src/test/java/br/com/mlpdleal/tasks/functional/TasksTest.java
@@ -23,7 +23,7 @@ public class TasksTest {
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
 
 		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.32:4444/wd/hub"), cap);
-		driver.get("http://localhost:8001/tasks/");
+		driver.get("http://192.168.0.32:8001/tasks/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		return driver;
